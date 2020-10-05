1cc2d733d2315626d8803c896ea43a605cf6e74a
hgoegheo <morimori2003@gmail.dom>
2020-10-05 16:03:46 +0900

    カメラのレーンをオブジェクトにめり込まないようにした
    ただ、レーンのポイントを移動させすぎたせいか、たまにガクガクッと視点orキャラクターがブレるようになってしまった。おそらく一から作り直した方が早い。

--- Assets/game/Camera/Camera_angle.cs
+++ Assets/game/Camera/Camera_angle.cs
@@ -5,35 +5,39 @@ using Cinemachine;
 
 public class Camera_angle : MonoBehaviour
 {
-    Camera_Collision CCscript;
-    GameObject camera_hanntei;
+    public Camera_Collision CameraC;
+    //public GameObject camera_hanntei;
+
+    string CCscript;
 
     void Start()
     {
-        camera_hanntei = GameObject.Find("camera_hanntei");
-        CCscript = camera_hanntei.GetComponent<Camera_Collision> ();
+        //camera_hanntei = GameObject.Find("camera_hanntei");
+        //CCscript = camera_hanntei.GetComponent<Camera_Collision> ();
 
         CinemachineVirtualCamera CVcamera = this.gameObject.GetComponent<CinemachineVirtualCamera>();
+
+        CCscript = CameraC.nani_hureta;
     }
 
 
     void Update()
     {
-        if (CCscript.nani_hureta == "hall_in")
+        if (CCscript == "hall_in")
         {
-            CVcamera.Priority = 40;
+            //CVcamera.Priority = 40;
             Debug.Log("Hello");
         }
-        if (CCscript.nani_hureta == "hall_out")
+        if (CCscript == "hall_out")
         {
             Debug.Log("Hello");
         }
 
-        if (CCscript.nani_hureta == "tuuro_in")
+        if (CCscript == "tuuro_in")
         {
             Debug.Log("Hello");
         }
-        if (CCscript.nani_hureta == "tuuro_out")
+        if (CCscript == "tuuro_out")
         {
             Debug.Log("Hello");
         }
---
+++ Assets/hacing/Hacking2/tiitle2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class tiitle2 : MonoBehaviour
+
+{
+    float count = 0.0f;
+    float timeLimit = 2.5f;
+    TextImput2 script;
+    GameObject PROGRAM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PROGRAM = GameObject.Find("PROGRAM");
+        script = PROGRAM.GetComponent<TextImput2>();
+        //CanvasGroup CG = this.GetComponent<CanvasGroup>();
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+
+        if (count >= timeLimit)
+        {
+            //CG.alpha = 0;
+            this.GetComponent<CanvasGroup>().alpha = 0;
+        }
+
+        if (script.pattern == "4")//シーンへの移行の処理。なんでこんなコード書いたんや...
+        {
+            this.GetComponent<CanvasGroup>().alpha = 1;
+
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
