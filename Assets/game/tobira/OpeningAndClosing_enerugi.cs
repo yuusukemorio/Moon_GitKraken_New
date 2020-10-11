4447236a173e01cd744ea263514fda46363efde4
yusuke <morimori2003@gmail.com>
2020-10-12 01:46:18 +0900

    OPと扉の微調整
    OPからプレイシーンへの移行を滑らかに、開かなかった扉を開くようにした。

---
+++ Assets/game/tobira/OpeningAndClosing_enerugi.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpeningAndClosing_enerugi : MonoBehaviour
+{
+    public float speed;
+    public float stop;
+    public string direction_LorR;
+
+    string BSscriptEnerugi;
+    public enerugi_scene bunnkitennEnerugi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BSscriptEnerugi = bunnkitennEnerugi.huretakaEnerugi;
+
+        if (BSscriptEnerugi == "yes")
+        {
+            if (direction_LorR == "L")
+            {
+                if (transform.position.x <= stop)
+                {
+                    transform.position = transform.position + new Vector3(speed, 0, 0);
+                }
+            }
+
+            else if (direction_LorR == "R")
+            {
+                if (transform.position.x >= stop)
+                {
+                    transform.position = transform.position + new Vector3(speed, 0, 0);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/game/tobira/enerugi_scene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enerugi_scene : MonoBehaviour
+{
+
+    public string huretakaEnerugi = "no";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "iv")
+        {
+            //ChangeScene();
+            huretakaEnerugi = "yes";
+            Debug.Log(huretakaEnerugi);
+        }
+    }
+}
+
