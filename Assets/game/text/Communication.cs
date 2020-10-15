0d5d872d121dd7719b6005395fe0c0eca0a52156
hgoegheo <morimori2003@gmail.dom>
2020-10-15 16:02:51 +0900

    textでセリフを管理しようとしてる
    textファイルを使ってセリフを管理しようとしてる。
    [,]の前と後ろを取得する方法がちょいよくわからん...

---
+++ Assets/game/text/Communication.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Communication : MonoBehaviour
+{
+    //[SerializeField]
+    public Text textLabel;
+   //[SerializeField]
+    public TextAsset textFile;
+
+    private string textData;
+    private string[] splitText;
+
+    int TextCount = 0;
+    float TimeCount = 0.0f;
+
+
+    void Start()
+    {
+        textData = textFile.text;
+
+        // 改行で分割して配列に代入
+        splitText = textData.Split(char.Parse("\n"));
+        // テキストデータの先頭文字列（１行目）を表示する
+        textLabel.text = splitText[0];
+    }
+
+
+    void Update()
+    {
+        TimeCount += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (TimeCount >= DisplayTime[textNumber])
+            {
+                Speaking();
+                TimeCount = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (TimeCount >= DisplayTime[textNumber])
+            {
+                Speaking();
+                TimeCount = 0;
+            }
+        }
+    }
+
+    void Speaking()
+    {
+        textLabel.text = splitText[TextCount];
+        TextCount = TextCount + 1;
+
+    }
+}
