f80a6922b626dd837daf79b1d92c5b30038d761c
hgoegheo <morimori2003@gmail.dom>
2020-10-16 12:16:41 +0900

    セリフの発火について悩んでる
    ちょっとガッツリ変化させるからとりあえずコミット

--- Assets/game/text/Communication.cs
+++ Assets/game/text/Communication.cs
@@ -15,40 +15,69 @@ public class Communication : MonoBehaviour
 
     float SpeakTime = 0.0f;
 
-
     int TextCount = 0;
     float TimeCount = 0.0f;
+
+
+    string CCscript;
+    public Camera_Collision CameraC;
 
 
     void Start()
     {
         textData = textFile.text;
-
-        // 改行で分割して配列に代入
-        splitText = textData.Split(char.Parse("\n"));
-        // テキストデータの先頭文字列（１行目）を表示する
-        textLabel.text = splitText[0];
+        splitText = textData.Split(char.Parse("\n"));// 改行で分割して配列に代入
     }
 
 
     void Update()
     {
+        CCscript = CameraC.nani_hureta;
         TimeCount += Time.deltaTime;
-        for (int x = 0; x <= 10; x++) //正直よろしくない処理。何回セリフを喋らすかをマジックナンバーを用いて手動で入力している。
+        Debug.Log(TimeCount);
+        if (CCscript == "Communication1")
         {
-            SpeakTime = float.Parse(splitText[TextCount].Substring(0, 3));//最初から三番目までの文字をint型に変換する。
-            if (TimeCount >= SpeakTime)
+            for (int x = 0; x <= 8; x++) //正直よろしくない処理。何回セリフを喋らすかをマジックナンバーを用いて手動で入力している。
             {
-                Speaking();
-                TimeCount = 0;
+                SpeakTime = float.Parse(splitText[TextCount].Substring(0, 3));//最初から三番目までの文字をint型に変換する。
+                textLabel.text = splitText[TextCount].Substring(4, splitText[TextCount].Length - 4);//配列からTextCoun番目の要素の4番目から全体の長さ-4した場所を求める
+
+                if (TimeCount >= SpeakTime)
+                {
+                    TextCount += 1;
+                    TimeCount = 0;
+                }
             }
         }
+
+        //Communication_IVSG("Communication1", 8);//OP,おはよ
+        //Communication_IVSG("Communication2", 12);//今日も...行くの?
+        //Communication_IVSG("Communication3", 18);//宇宙服の点検嫌!&アナログっていいよね
+        //Communication_IVSG("Communication4", 6);//エネルギー残量大丈夫?
+        //Communication_IVSG("Communication5", 34);//スグって読むねんで&分析家結果&映画のセリフ言い合い
+        //Communication_IVSG("Communication6", 3);//地下街やな嫌いやわ...
+        //Communication_IVSG("Communication7", 6);//ネオン消えへんな...
+        //Communication_IVSG("Communication8", 10);//核兵器の記録見つかったで
+        //Communication_IVSG("Communication9", 9);//ED,最深部ついたぜ!
+
     }
 
-    void Speaking()
+    void Communication_IVSG(string a, int b)
     {
-        textLabel.text = splitText[TextCount];//textに表示
-        TextCount = TextCount + 1;//セリフの順番
+        if (CCscript == a)
+        {
+            TimeCount = 0;
+            for (int x = 0; x <= b; x++) //正直よろしくない処理。何回セリフを喋らすかをマジックナンバーを用いて手動で入力している。
+            {
+                SpeakTime = float.Parse(splitText[TextCount].Substring(0, 3));//最初から三番目までの文字をint型に変換する。
+                textLabel.text = splitText[TextCount].Substring(4, splitText[TextCount].Length - 4);//配列からTextCoun番目の要素の4番目から全体の長さ-4した場所を求める
 
+                if (TimeCount >= SpeakTime)
+                {
+                    TextCount += 1;
+                    TimeCount = 0;
+                }
+            }
+        }
     }
 }
---
+++ Assets/game/text/Communication_hanntei.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Communication_hanntei : MonoBehaviour
+{
+
+    public string nani_hureta;
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        //Debug.Log(other.gameObject.name);
+        nani_hureta = other.gameObject.name;
+    }
+}
